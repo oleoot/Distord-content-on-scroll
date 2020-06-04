7c5ee5fe55e4dd176268b3ad241184721b41bc9e
oleoot <oleoot28@gmail.com>
2020-06-04 12:38:55 +0300

    Js done

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const content = document.querySelector("section");
+let currentPos = window.pageYOffset;
+
+const callDistort = function () {
+    const newPos = window.pageYOffset;
+    const diff = newPos - currentPos;
+    const speed = diff * 0.35;
+
+    content.style.transform = "skewY(" + speed + "deg)";
+    currentPos = newPos;
+    requestAnimationFrame(callDistort);
+};
+
+callDistort();
